ecc98aee7fdedbf80f604e77cd47650658653241
Percy Michael <percymichael0@gmail.com>
2024-11-25 19:49:19 +0300

    fix db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->foreignId('role')->nullable();
+            $table->string('role')->nullable();
             $table->string('reset_code')->nullable();
             $table->timestamp('reset_code_expires_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
